e30a869a743d067b9ef44852731e0bde32cd5ff9
Kim Andersson <Kimpabooy@live.com>
2024-11-05 14:48:59 +0100

    made classes and methods

---
+++ Labb3 Polymorphism/Circle.cs
@@ -0,0 +1,20 @@
+﻿namespace Labb3_Polymorphism
+{
+    public class Circle : Geometry
+    {
+        public double Radius { get; set; }
+        public Circle()
+        {
+            Radius = 24;
+            Shape = "Cirkel";
+        }
+        
+        // Area = π (r x r)
+        public override double Area() => Math.PI * Radius * Radius;
+        
+        //public override double Area()
+        //{
+        //    return Math.PI * Radius * Radius;
+        //}
+    }
+}
---
+++ Labb3 Polymorphism/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb3_Polymorphism
+{
+    public class Rectangle : Geometry
+    {
+        public double Base { get; set; }
+        public double Hight { get; set; }
+        public Rectangle()
+        {
+            Base = 5;
+            Hight = 3;
+            Shape = "Rektangel";
+        }
+        // Area = Bas * Höjd
+        public override double Area() => Base * Hight;
+    }
+}
